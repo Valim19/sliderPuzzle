7e34e6ab94dcb583360413477d358d9fba549e0e
Anna Valim <annacarolinavalim@gmail.com>
2024-10-11 17:44:52 -0300

    completando Board.java

--- Board.java
+++ Board.java
@@ -9,38 +9,105 @@
  * You may not call any library functions other those in java.lang, java.util, and algs4.jar.
  */
 
+import java.util.Arrays;
+
 public class Board {
+    private final int BOARD_DIMENSION;
+    private final int[][] BOARD;
 
-    // create a board from an n-by-n array of tiles,
-    // where tiles[row][col] = tile at (row, col)
-    public Board(int[][] tiles)
+    public Board(int[][] tiles) {
+        BOARD_DIMENSION = tiles.length;
 
-    // string representation of this board
-    public String toString()
+        BOARD = copyBoardContent(tiles);
+    }
 
-    // board dimension n
-    public int dimension()
+    public String toString() {
+        StringBuilder str = new StringBuilder();
 
-    // number of tiles out of place
-    public int hamming()
+        str.append(BOARD_DIMENSION).append("\n");
 
-    // sum of Manhattan distances between tiles and goal
-    public int manhattan()
+        for (int row = 0; row < BOARD_DIMENSION; row++) {
+            for (int col = 0; col < BOARD_DIMENSION; col++)
+                str.append(String.format("%2d", BOARD[row][col]));
 
-    // is this board the goal board?
-    public boolean isGoal()
+            str.append("\n");
+        }
 
-    // does this board equal y?
-    public boolean equals(Object y)
+        return str.toString();
+    }
 
-    // The neighbors() method returns an iterable containing the neighbors of the board.
-    // Depending on the location of the blank square, a board can have 2, 3, or 4 neighbors.
-    public Iterable<Board> neighbors()
+    public int dimension() {
+        return BOARD_DIMENSION;
+    }
+
+    public int hamming() {
+        int totalDistance = 0;
+
+        for (int row = 0; row < BOARD_DIMENSION; row++) {
+            for (int col = 0; col < BOARD_DIMENSION; col++) {
+                int expectedValue = calculationHamming(row, col);
+
+                if (expectedValue != 0 && expectedValue != BOARD[row][col])
+                    totalDistance++;
+            }
+        }
+
+        return totalDistance;
+    }
+
+    public int manhattan() {
+        int distance = 0;
+
+        for (int row = 0; row < BOARD_DIMENSION; row++)
+            for (int col = 0; col < BOARD_DIMENSION; col++)
+                distance += calculationManhattan(BOARD[row][col], row, col);
+
+        return distance;
+    }
+
+    public boolean isGoal() {
+        return hamming() == 0;
+    }
+
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+
+        Board otherBoard = (Board) other;
+
+        return Arrays.deepEquals(this.BOARD, otherBoard.BOARD);
+    }
+
+    public Iterable<Board> neighbors() {
+
+    }
 
     // a board that is obtained by exchanging any pair of tiles
-    public Board twin()
+    public Board twin() {
 
-    // unit testing (not graded)
-    public static void main(String[] args)
+    }
 
+    private int[][] copyBoardContent(int[][] original) {
+        int[][] destination = new int[BOARD_DIMENSION][BOARD_DIMENSION];
+
+        for (int row = 0; row < BOARD_DIMENSION; row++)
+            System.arraycopy(original[row], 0, destination[row], 0, BOARD_DIMENSION);
+
+        return destination;
+    }
+
+    private int calculationHamming(int row, int col) {
+        if (row == (BOARD_DIMENSION - 1) && col == (BOARD_DIMENSION - 1)) return 0;
+        return row * BOARD_DIMENSION + col + 1;
+    }
+
+    private int calculationManhattan(int boardElement, int row, int col) {
+        if (boardElement == 0) return 0;
+
+        int correctRow = (boardElement - 1) / BOARD_DIMENSION;
+        int correctCol = (boardElement - 1) % BOARD_DIMENSION;
+
+        return Math.abs(row - correctRow) + Math.abs(col - correctCol);
+    }
 }
